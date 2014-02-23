80a130f2ea6b29e185bdea9736a5053a04ec9e37
JMPerez <jmperez1985@gmail.com>
2014-02-23 09:02:00 +0100

    Fixed Google for Album & Artist search

--- js/providers/google.js
+++ js/providers/google.js
@@ -4,45 +4,87 @@ SP.Providers.Google = function() {
     function supports(url) {
       return url.indexOf('www.google.') != -1;
     }
-    function detect(callbackFound, clallbackNotFound) {
 
-      setTimeout(function() {
-        // see if this an artist page
-        if ($('.kno-sh.ellip').text().indexOf('Songs') != -1 || $('.kno-sh.ellip').text().indexOf('Upcoming events') != -1) {
-          // fetch artist name
-          var artist = $('.kno-ecr-pt').text();
-          SP.Search.searchArtist(artist, function(href) {
-            // fetch album by that artist
-            $.ajax({
-              'url': 'http://ws.spotify.com/lookup/1/.json?uri=' + href + '&extras=album'
-            }).done(function(data) {
-              if (data.artist.albums.length > 0) {
-                data.artist.albums.slice(0, 1).forEach(function(a) {
-                  $('<iframe src="https://embed.spotify.com/?uri=' + a.album.href + '" width="250" height="80" frameborder="0" allowtransparency="true"></iframe>')
-                    .prependTo('.kno-desc');
-                });
-              } else {
-                clallbackNotFound();
-              }
-          }, function() {
-            clallbackNotFound();
-          });
-        });
+
+    var ResultTypes = {
+      artist: 0,
+      album: 1,
+      track: 2,
+      other: -1
+    };
+
+    function getResultType() {
+      // get type
+      var resultType = $('.kno-result ._Gr').text();
+      if (resultType.indexOf('followers on Google+') !== -1 &&
+          $('._Ve.kno-sh').text().indexOf('Songs') !== -1) {
+        return ResultTypes.artist;
       } else {
-        if ($('.kltb-tr.selected').length) {
-          var trackName = $('.kltb-tr.selected .kltb-a').text();
-          var artistName = $('.klptitle.ellip').eq(0).text();
-          SP.Search.searchTrack(trackName + ' - ' + artistName, function(href) {
-            $('.appcenter iframe').remove();
-            $('<iframe style="position: absolute;left: 741px;top: 33px;" src="https://embed.spotify.com/?uri=' + href + '" width="250" height="80" frameborder="0" allowtransparency="true"></iframe>')
-              .prependTo('.appcenter');
-            }, function() {
-              clallbackNotFound();
-            });
-        } else {
-          clallbackNotFound();
+        switch (resultType) {
+          case 'Band':
+            return ResultTypes.artist;
+          case 'Musical Album':
+            return ResultTypes.album;
+          default:
+            return ResultTypes.other;
         }
       }
+    }
+
+    function getName() {
+      var name = $('.kno-result .kno-ecr-pt').text();
+      return name;
+    }
+
+    function getPlaceholder() {
+      return $('.kno-fb-ctx._Wv');
+    }
+
+    function detect(callbackFound, callbackNotFound) {
+
+      setTimeout(function() {
+
+        var resultType = getResultType(),
+            name = getName();
+        switch (resultType) {
+          case ResultTypes.artist:
+            SP.Search.searchArtist(name, function(href) {
+              // fetch album by that artist
+              $.ajax({
+                'url': 'http://ws.spotify.com/lookup/1/.json?uri=' + href + '&extras=album'
+              }).done(function(data) {
+                if (data.artist.albums.length > 0) {
+                  data.artist.albums.slice(0, 1).forEach(function(a) {
+                    $('<iframe src="https://embed.spotify.com/?uri=' + a.album.href + '" width="250" height="80" frameborder="0" allowtransparency="true"></iframe>')
+                      .appendTo(getPlaceholder());
+                  });
+                } else {
+                  callbackNotFound();
+                }
+              }, function() {
+                callbackNotFound();
+              });
+            });
+            break;
+
+          case ResultTypes.album:
+            SP.Search.searchAlbum(name, function(href) {
+              // fetch album by that artist
+              $.ajax({
+                'url': 'http://ws.spotify.com/lookup/1/.json?uri=' + href
+              }).done(function(data) {
+                if (data.album) {
+                  $('<iframe src="https://embed.spotify.com/?uri=' + data.album.href + '" width="250" height="80" frameborder="0" allowtransparency="true"></iframe>')
+                    .appendTo(getPlaceholder());
+                } else {
+                  callbackNotFound();
+                }
+              }, function() {
+                callbackNotFound();
+              });
+            });
+            break;
+        }
     }, 1000);
   }
   return {
